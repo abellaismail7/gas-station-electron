1fea57858a5f878cff6727c5cefa517a4d2e988a
Ismail A Bella <1ndroidbella@gmail.com>
2021-01-25 16:20:52 +0000

    add antdv ui framework

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,21 @@
 const {remote} = window.require("electron")
+import {
+    Layout,
+    Button,
+    Tabs,
+    Menu,
+    Modal,
+    Table,
+    Form,
+    InputNumber,
+    Tag,
+    PageHeader,
+    Icon,
+    Input,
+    Popconfirm,
+    message
+  } from 'ant-design-vue';
+
 import Knex from "knex";
 import Vue from "vue";
 import App from "./App.vue";
@@ -6,8 +23,24 @@ import router from "./router";
 import store from "./store";
 
 Vue.prototype.$db = remote.getGlobal("$db")
+Vue.prototype.$message = message
 
 Vue.config.productionTip = false;
+
+Vue.use(Layout);
+Vue.use(Tabs);
+Vue.use(Button);
+Vue.use(Table);
+Vue.use(Form);
+Vue.use(InputNumber);
+Vue.use(Input);
+Vue.use(Menu);
+Vue.use(Modal);
+Vue.use(Tag);
+Vue.use(Icon);
+Vue.use(Popconfirm);
+
+Vue.use(PageHeader);
 
 
 new Vue({
